e9048d3b95967880a7e8e56bb8485d847e4088ef
Sachin Kumar Rajput <skr571999@gmail.com>
2023-08-30 14:42:36 +0530

    v.1.0.4

--- archive/try/index.js
+++
@@ -1,3 +0,0 @@
-const { version } = require('./package.json');
-
-console.log(`Hello in world of GitHub Packages v-${version}`);
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const { version } = require('./package.json');
 
 console.log(`Hello, World! of GitHub Packages- v-${version}`);
+
+export const sum = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => {
+  return a - b;
+};
+
+export const random = () => {
+    return Math.random()
+}

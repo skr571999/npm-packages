dd1834019f1ce548fde40ad86944b3f708931426
Sachin Kumar Rajput <skr571999@gmail.com>
2023-08-30 16:24:23 +0530

    add version

--- packages/mongoose-utils/lib/mongoose-utils.js
+++ packages/mongoose-utils/lib/mongoose-utils.js
@@ -2,6 +2,9 @@
 
 module.exports = mongooseUtils;
 
+const { version } = require('./../package.json');
+
 function mongooseUtils() {
+  console.log(`mongoose-utils : V  ${version}`);
   return 'Hello from mongooseUtils';
 }

22181792e1d7964133c96d0e5cd957a1cd14bca5
Sachin Kumar Rajput <skr571999@gmail.com>
2023-08-31 22:45:08 +0530

    move to main project

--- packages/express-mongoose-utils/lib/resources/permission/permission.service.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_utils_1 = require("@skrmain/mongoose-utils");
-const permission_models_1 = require("./permission.models");
-class PermissionService extends mongoose_utils_1.DbService {
-}
-exports.default = new PermissionService(permission_models_1.PermissionModel);
--- packages/express-mongoose-utils/lib/resources/permission/permission.service.ts
+++
@@ -1,7 +0,0 @@
-import { DbService } from '@skrmain/mongoose-utils';
-
-import { PermissionModel } from './permission.models';
-
-class PermissionService<T> extends DbService<T> {}
-
-export default new PermissionService(PermissionModel);

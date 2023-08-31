e3eb4c17466b3a1826e0257ae8d9cb2b0730feb5
Sachin Kumar Rajput <skr571999@gmail.com>
2023-08-31 21:43:58 +0530

    [express-mongoose-utils] add build config

---
+++ packages/express-mongoose-utils/lib/resources/permission/permission.service.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_utils_1 = require("@skrmain/mongoose-utils");
+const permission_models_1 = require("./permission.models");
+class PermissionService extends mongoose_utils_1.DbServiceWrapper {
+}
+exports.default = new PermissionService(permission_models_1.PermissionModel);
